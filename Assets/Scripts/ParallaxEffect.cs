3231f94663489a5f494f9548a69a630c2598bf4c
Jolteous <113569013+Jolteous@users.noreply.github.com>
2023-07-18 13:20:53 -0700

    Parallax Effect added

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+     private float startingPos; 
+    private float lengthOfSprite;  
+    public float AmountOfParallax; 
+    public Camera MainCamera; 
+    // Start is called before the first frame update
+    void Start()
+    {
+            startingPos = transform.position.x;    
+            lengthOfSprite = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Vector3 Position = MainCamera.transform.position;
+            float Temp = Position.x * (1 - AmountOfParallax);
+            float Distance = Position.x * AmountOfParallax;
+
+            Vector3 NewPosition = new Vector3(startingPos + Distance, transform.position.y, transform.position.z);
+
+            transform.position = NewPosition;
+    }
+}
